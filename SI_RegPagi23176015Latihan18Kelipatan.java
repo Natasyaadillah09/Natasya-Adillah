2d2d2f0d311fe34c9d6d5246d237c713fddb0db9
Natasyaadillah09 <ntsyaadllh@gmail.com>
2024-10-31 11:05:35 +0700

    Add files via upload

---
+++ SI_RegPagi23176015Latihan18Kelipatan.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package si_regpagi.pkg23176015.latihan18.kelipatan;
+
+/**
+ *
+ * @author Asus
+ * NAMA     : Natasya Adillah
+ * KELAS    : PBO 06
+ * NIM      : 23176015
+ * DESKRIPSI PROGRAM : Program ini untuk menampilkan kelipatan 3.5 dalam 
+ * rentang tertentu (dari 3.5 hingga 35)
+ */
+public class SI_RegPagi23176015Latihan18Kelipatan {
+    public static void main(String[] args) {
+        double angka = 3.5;
+        while (angka <= 35){
+            System.out.println(angka);
+            angka += 3.5;
+        }
+    }
+}
